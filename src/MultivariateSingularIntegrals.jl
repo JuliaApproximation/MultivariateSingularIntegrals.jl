5eeaa6d7bdd27bf298d9c5dd256dbc8f003cce5f
Sheehan Olver <solver@mac.com>
2025-04-24 14:44:24 +0100

    Use accurate implementation

--- src/MultivariateSingularIntegrals.jl
+++ src/MultivariateSingularIntegrals.jl
@@ -2,111 +2,24 @@ module MultivariateSingularIntegrals
 using LinearAlgebra, ClassicalOrthogonalPolynomials, SingularIntegrals, FillArrays
 import Base: size
 
-export logkernelsquare, logkernelsquare!
+export logkernelsquare
 
 """
-    L₀₀(z) = ∫_{-1}^1∫_{-1}^1 log(z-(s+im*t))dsdt 
+    zlog(z)
+
+implements ``z*log(z)``.
 """
-L₀₀(z) = (1-z)*M0(z-1) + im*M1(z-1) + (1+z)*M0(z+1) - im*M1(z+1) - 4
-
-#computes [a b; c d] \ [1,0]
-@inline function twobytwosolve_1(a, b, c, d)
-    dt = inv(muladd(a,d,-b*c))
-    d*dt, -c*dt
-end
-
-#computes [a b; c d] \ [0, 1]
-@inline function twobytwosolve_2(a, b, c, d)
-    dt = inv(muladd(a,d,-b*c))
-    -b*dt, a*dt
-end
-
-
-
-
-"""
-    biforwardsub!(M, A, B, R1, R2)
-
-finds `X` such that `A*X + im*X*B' = R1` and `B*X + im*X*A' = R2`
-"""
-function biforwardsub!(M, z, A, B, R1, R2)
-    m,n = size(M)
-    begin
-        # fill out first row
-        a,b = twobytwosolve_2(A[1,2], B[1,2], im*B[1,2], im*A[1,2])
-        M[1,2] = ((a+b)*z- (a+im*b)*(A[1,1]-im*B[1,1]))*M[1,1] - a*R1[1,1] - b*R2[1,1]
-        for j = 2:n-1
-            a,b = twobytwosolve_2(A[1,2], B[1,2], im*B[j,j+1], im*A[j,j+1])
-            M[1,j+1] = ((a+b)*z - a*A[1,1] + b*B[1,1])*M[1,j] - im*(a*B[j,j-1] + b*A[j,j-1])*M[1,j-1] - a*R1[1,j] - b*R2[1,j]
-        end
-
-        # fill out first column
-        a,b = twobytwosolve_1(A[1,2], B[1,2], im*B[1,2], im*A[1,2])
-        M[2,1] = ((a+b)*z- (a+im*b)*(A[1,1]-im*B[1,1]))*M[1,1] - a*R1[1,1] - b*R2[1,1]
-        for k = 2:m-1
-            a,b = twobytwosolve_1(A[k,k+1], B[k,k+1], im*B[1,2], im*A[1,2])
-            M[k+1,1] = ((a+b)*z - im*(a*B[1,1] + b*A[1,1]))*M[k,1] - (a*A[k,k-1] + b*B[k,k-1])*M[k-1,1] - a*R1[k,1] - b*R2[k,1]
-        end
-
-        # fill out 2nd column from first
-        for k = 2:m
-            a,b = twobytwosolve_2(A[k,k+1], B[k,k+1], im*B[1,2], im*A[1,2])
-            M[k,2] = (a+b)*z*M[k,1] - (a*A[k,k-1] + b*B[k,k-1])*M[k-1,1] - (im*a*B[1,1] + im*b*A[1,1])*M[k,1] - a*R1[k,1] - b*R2[k,1]
-        end
-
-        # fill out rest, column-by-column
-        for k = 2:m, j = 2:n-1
-            a,b = twobytwosolve_2(A[k,k+1], B[k,k+1], im*B[j,j+1], im*A[j,j+1])
-            M[k,j+1] = (a+b)*z*M[k,j] - (a*A[k,k-1] + b*B[k,k-1])*M[k-1,j] - (im*a*B[j,j-1] + im*b*A[j,j-1])*M[k,j-1]- a*R1[k,j] - b*R2[k,j]
-        end
-    end
-    
-    M
-end
-
-function m_const_vec(n, z)
-    (x,y) = reim(z)
-    T = float(real(typeof(z)))
-    if x > 0 || y ≥ 1 || (x == 0 && !signbit(x) && y < 0) # need to treat im*y+0.0 differently
-        [im*convert(T,π); Zeros{T}(n-1)]
-    elseif y ≤ -1
-        [-3im*convert(T,π); Zeros{T}(n-1)]
-    else # x ≤ 0 && -1 ≤ y ≤ 1
-        r = Weighted(Jacobi{T}(1,1))[y,1:n-1]
-        [((1 + y) - 3*(1-y))/2*im*convert(T,π); -im .* convert(T,π) .* r ./ (1:(n-1))]
-    end
-end
-
-function imlogkernel_vec(n, z)
-    T = float(real(typeof(z)))
-    transpose(complexlogkernel(Legendre{T}(), -im*float(z)))[1:n] + m_const_vec(n, float(z))
-end
-
-function rec_rhs_k!(R, z)
-    n = size(R,1)
-    T = float(real(typeof(z)))
-    P = Legendre{T}()
-    x = axes(P,1)
-    Mm1 = imlogkernel_vec(n+1, z+1)
-    fill!(R, zero(T))
-    R[1,1] = -2im*Mm1[2] +2*(z+1)*Mm1[1]-4
-    R[2,1] = -convert(T,4)/3
-    for j = 1:n-1
-        R[1,j+1] = - 2im * convert(T,j+1)/(2j+1) * Mm1[j+2] + 2*(1+z)*Mm1[j+1]  - 2im * convert(T,j)/(2j+1) * Mm1[j]
-    end
-    R
-end
-
-rec_rhs_j!(R, z) = -1 ≤ real(z) ≤ 1 && -1 ≤ imag(z) ≤ 1 ? rec_rhs_j_insquare!(R, z) : rec_rhs_j_offsquare!(R, z)
-
-
 zlog(z) = iszero(z) ? zero(z) : z*log(z)
+
+"""
+    zlogm(z)
+
+implements ``z*log(z)`` but taking the other choice on the branch cut.
+"""
+zlogm(z) = iszero(imag(z)) && real(z) < 0 ? -zlog(abs(z)) - im*π*z : zlog(z)
 
 L0(z) = zlog(1 + complex(z)) - zlog(complex(z)-1) - 2one(z)
 L1(z, r0=L0(z)) = (z+1)*r0/2 + 1 - zlog(complex(z)+1)
-
-M0(z) = L0(-im*float(z)) + m_const(0, float(z))
-M1(z, r0=L0(-im*z)) = L1(-im*float(z), r0) + m_const(1, float(z))
 
 function m_const(k, z)
     (x,y) = reim(z)
@@ -126,103 +39,149 @@ function m_const(k, z)
     end
 end
 
+M0(z) = L0(-im*float(z)) + m_const(0, float(z))
+M1(z, r0=L0(-im*z)) = L1(-im*float(z), r0) + m_const(1, float(z))
 
-function rec_rhs_j_offsquare!(R, z)
-    n = size(R,1)
-    x,y = reim(z)
+
+"""
+    L₀₀(z) = ∫_{-1}^1∫_{-1}^1 log(z-(s+im*t))dsdt 
+"""
+L₀₀(z) = (1-z)*M0(z-1) + im*M1(z-1) + (1+z)*M0(z+1) - im*M1(z+1) - 4
+
+function m_const_vec(n, z)
+    (x,y) = reim(z)
     T = float(real(typeof(z)))
-    P = Legendre{T}()
-    Lm1 = transpose(complexlogkernel(P, z+im))[1:n+1] # transpose makes col-vector, works around slowess
-    μ = m_const_vec(n, z)
-    fill!(R, zero(T))
-    R[1,1] = - 2Lm1[2]  - 4im*m_const(1,z)+  2*(z+im)*(M0(1-im*z) + m_const0(z)) - 4im
-    R[1,2] = -(convert(T,4)*im)/3 + 2x*m_const(1, z)
-    for j = 3:n
-        R[1,j] = 2x*μ[j]
+    if x > 0 || y ≥ 1 || (x == 0 && !signbit(x) && y < 0) # need to treat im*y+0.0 differently
+        [im*convert(T,π); Zeros{T}(n-1)]
+    elseif y ≤ -1
+        [-3im*convert(T,π); Zeros{T}(n-1)]
+    else # x ≤ 0 && -1 ≤ y ≤ 1
+        r = Weighted(Jacobi{T}(1,1))[y,1:n-1]
+        [((1 + y) - 3*(1-y))/2*im*convert(T,π); -im .* convert(T,π) .* r ./ (1:(n-1))]
     end
-    R[2,1] = - convert(T,4)/3 * Lm1[3] - convert(T,2)/3 *M0(1-im*z) - convert(T,2)/3 *m_const0(z) + 2*(z+im)*Lm1[2]
-    for j = 2:n
-        R[2,j] = -2μ[j]/3
-    end
-    for k = 2:n-1
-        R[k+1,1] = 2(im+z)*Lm1[k+1] - convert(T,2k)/(2k+1)*Lm1[k] - convert(T,2(k+1))/(2k+1)*Lm1[k+2]
-    end
-    R
+end
+
+function imlogkernel_vec(n, z)
+    T = float(real(typeof(z)))
+    transpose(complexlogkernel(Legendre{T}(), -im*float(z)))[1:n] + m_const_vec(n, float(z))
 end
 
 
 
-# averages the difference between M(k,z-s) and L(k,-im*(z-s))
-function m_const_square_0_vec(z, Wx)
+function rec_rhs_1(z, m, n)
     x,y = reim(z)
-    T = float(typeof(x))
-    n = length(Wx)
-    [im*convert(T,π)*((1+x) + (1-x) * ((1 + y) - 3*(1-y))/2); 
-        (im*convert(T,π)* (-1 + ((1 + y) - 3*(1-y))/2)) .* Wx ./ (2*(1:n))]
-end
-
-
-function rec_rhs_j_insquare!(R, z)
-    n = size(R,1)
-    T = float(real(typeof(z)))
-    x,y = reim(z)
-    W = Weighted(Jacobi{T}(1,1))
-    P = Legendre{T}()
-    Wy = W[y,1:n-1]
-    Wx = W[x,1:n]
-    Vx = Weighted(Jacobi{T}(2,2))[x,1:n-2]
-    Lm1 = transpose(complexlogkernel(P, z+im))[1:n+1] # transpose makes col-vector, works around slowess
+    T= complex(float(typeof(z)))
     πT = convert(T, π)
-    μ0 = m_const_square_0_vec(z, Wx)
-
-    R[3:end,2:end] .= (im * πT) .*Vx ./ (2 .* (2:n-1) .* (1:(n-2))) .* (Wy ./ (2 .* (1:n-1)))'
-    for k = 2:n-1
-        R[k+1,1] = - convert(T,2k)/(2k+1) * Lm1[k] + 2im*Lm1[k+1] - convert(T,2(k+1))/(2k+1) * Lm1[k+2] +
-              - convert(T,k+1)/(2k+1) * μ0[k+2] - convert(T,k)/(2k+1) * μ0[k] +
-              2z*Lm1[k+1] + (z+im)*μ0[k+1] - Wx[k]/k * πT*Wy[1]
+    F = zeros(T,m,n)
+    if x < -1 && -1 < y < 1
+        C_y = Ultraspherical{T}(-1/2)[y,2:n+1]
+        F[1,:] .-=  (4im*πT * x) .* C_y
+    elseif x < 1 && -1 < y < 1
+        C_y = Ultraspherical{T}(-1/2)[y,2:n+1]
+        F[1,:] .-=  (2im*πT * (x-1)) .* C_y
     end
-    for j = 2:n
-        R[2,j] = (im*πT/6 * (x^2 + x - 2)*(x-1)) * Wy[j-1] / (j-1)
+
+    F[1,1] += zlog(z-1-im) + zlogm(z-1+im) + zlog(z+1-im) + zlogm(z+1+im)
+    F[1,2] -= 4im/convert(T,3)
+
+    M₋ = imlogkernel_vec(n+1, z-1)
+    M₊ = imlogkernel_vec(n+1, z+1)
+
+    F[1,1] += im*(M₊[2] + M₋[2])
+    for j = 1:n-1
+        F[1,j+1] += im*(M₊[j+2] + M₋[j+2] - M₊[j] - M₋[j])/(2j+1)
     end
-    for j = 3:n
-        R[1,j] = (im*πT*(x-1)^2) * Wy[j-1] / (2*(j-1))
+    F[2,1] -= 4/convert(T,3)
+    F
+end
+
+
+
+function rec_rhs_2(z, m, n)
+    x,y = reim(z)
+    T= T= complex(float(typeof(z)))
+    πT = convert(T, π)
+    if -1 < x < 1 && -1 ≤ y < 1
+        C_x = Ultraspherical{T}(-3/2)[x,3:m+2]
+        C_y = Ultraspherical{T}(-1/2)[y,2:n+1]
+        F = (2im*πT) .* (C_x .* C_y') ./ 3
+        F[1,:] .-= (2im*πT) .* x .* C_y
+        F[2,:] .+= (2im*πT/3) .* C_y
+    elseif x ≤ -1 && -1 ≤ y < 1
+        F = zeros(T,m,n)
+        C_y = Ultraspherical{T}(-1/2)[y,2:n+1]
+        F[1,:] .= (-4im*πT) .* x .* C_y
+        F[2,:] .= (4im*πT/3) .*  C_y
+    else
+        F = zeros(T,m,n)
     end
-    R[1,2] = -convert(T,4)im/3 + im*πT*(1-x)^2/2 * Wy[1]
-    R[2,1] = -4Lm1[3]/3 - 2μ0[3]/3 - 2M0(1-im*z)/3 - μ0[1]/3 - πT*Wx[1] * Wy[1] + 2*(z+im)*Lm1[2] + (z+im)*μ0[2]
-    R[1,1] = -2Lm1[2] - μ0[2] + 2(z+im)*M0(1-im*z) - 2*πT*(1-x)*W[y,1] + (z+im)*μ0[1] - 4im
-    R
+
+    L₋ = complexlogkernel(Legendre{T}(), z-im)[1:m+1]
+    L₊ = complexlogkernel(Legendre{T}(), z+im)[1:m+1]
+
+    F[1,1] += L₋[2] + L₊[2] + zlog(z-im-1) + zlog(z-im+1) + zlog(z+im-1) + zlog(z+im+1)
+    F[1,2] -= 4im/convert(T,3)
+    for k = 1:m-1
+        F[k+1,1] += (L₊[k+2] + L₋[k+2] - L₊[k] - L₋[k])/(2k+1)
+    end
+    F[2,1] -= 4/convert(T,3)
+    F
 end
 
 
-
-struct LogKernelSquareData{T}
-    A::Tridiagonal{T, Vector{T}}
-    B::Tridiagonal{T, Vector{T}}
-    X::Matrix{Complex{T}}
-    F::Matrix{Complex{T}} # A*X + im*X*B' == F
-    G::Matrix{Complex{T}} # B*X + im*X*A' == G
+function logkernelsquare_populatefirstcolumn!(A, z, F_1=rec_rhs_1(z, size(A)...), F_2=rec_rhs_2(z, size(A)...))
+    A[2,1] = z * A[1,1]/3 + (F_2[1,1] - 2F_1[1,1])/3
+    for k = 1:size(A,1)-2
+        A[k+2,1] = (2k+1) * z * A[k+1,1]/(k+3) - (k-2)*A[k,1]/(k+3) + (2k+1) * (F_2[k+1,1] - 2F_1[k+1,1])/(k+3)
+    end
+    A
 end
 
-size(L::LogKernelSquareData, k...) = size(L.X, k...)
-
-function LogKernelSquareData{T}(n) where T
-    A = Tridiagonal((zero(T):n-1) ./ (3:2:(2n+1)), [-1; zeros(T, n)],  (convert(T,2):(1+n)) ./ (1:2:(2n)))
-    B = Tridiagonal((one(T):n) ./ (3:2:(2n+1)), zeros(T, n+1),  (one(T):n) ./ (1:2:(2n)))
-    X = Matrix{Complex{T}}(undef, n, n)
-    F = Matrix{Complex{T}}(undef, n, n)
-    G = Matrix{Complex{T}}(undef, n, n)
-    LogKernelSquareData(A, B, X, F, G)
+function logkernelsquare_populatefirstrow!(A, z, F_1=rec_rhs_1(z, size(A)...), F_2=rec_rhs_2(z, size(A)...))
+    A[1,2] = z*A[1,1]/(3im) + (F_1[1,1]-2F_2[1,1])/(3im)
+    for j = 1:size(A,2)-2
+        A[1,j+2] = -im * (2j+1) * z * A[1,j+1]/(j+3) - (j-2)*A[1,j]/(j+3) - im * (2j+1) * (F_1[1,j+1] - 2F_2[1,j+1])/(j+3)
+    end
+    A
 end
 
-LogKernelSquareData(n) = LogKernelSquareData{Float64}(n)
 
-logkernelsquare(z, n) = logkernelsquare!(LogKernelSquareData{float(real(typeof(z)))}(n), z)
-function logkernelsquare!(data, z)
-    R1 = rec_rhs_k!(data.F, z)
-    R2 = rec_rhs_j!(data.G, z)
+"""
+    logkernelsquare(z, n)
 
-    data.X[1,1] = L₀₀(z)
-    biforwardsub!(data.X, z, data.A, data.B, R1, R2)
+computes the matrix with entries ``∫_{-1}^1∫_{-1}^1 log(z-(s+im*t))P_k(s)P_j(t)dsdt`` up to total degree ``n``.
+The bottom right of the returned matrix is zero. For a square truncation compute `logkernelsquare(z,2n-1)[1:n,1:n]`.
+"""
+
+function logkernelsquare(z, n)
+    F_1 = rec_rhs_1(z, n, n)
+    F_2 = rec_rhs_2(z, n, n)
+    A = zeros(complex(float(eltype(z))),n,n)
+    A[1,1] = L₀₀(z)
+    logkernelsquare_populatefirstcolumn!(A, z, F_1, F_2)
+    logkernelsquare_populatefirstrow!(A, z, F_1, F_2)
+    F = F_2 - F_1
+
+    # 2nd row/column
+
+    for k = 1:n-2
+        A[k+1,2] = im*(F[k+1,1] + (A[k,1] - A[k+2,1])/(2k+1))
+    end
+
+    for j = 2:n-2
+        A[2,j+1] = F[1,j+1] + im*(A[1,j+2] - A[1,j])/(2j+1)
+    end
+
+    for ℓ = 1:((n-1)÷2-1)
+        for k = ℓ+1:n-(ℓ+2)
+            A[k+1,ℓ+2] = (2ℓ+1)*im*(F[k+1,ℓ+1] + (A[k,ℓ+1] - A[k+2,ℓ+1])/(2k+1)) + A[k+1,ℓ]
+        end
+        for j = ℓ+2:n-(ℓ+2)
+            A[ℓ+2,j+1] = (2ℓ+1) * (F[ℓ+1,j+1] + im*(A[ℓ+1,j+2] - A[ℓ+1,j])/(2j+1)) + A[ℓ,j+1]
+        end
+    end
+    A
 end
+
 
 end # module MultivariateSingularIntegrals
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
-using MultivariateSingularIntegrals, Test
-
-z = 0.1 + 0.1im
-n = 10
-logkernelsquare(z, n)
+using MultivariateSingularIntegrals, QuadGK, ClassicalOrthogonalPolynomials, Test
 
 
+Z = (2, 2im, 2+2im, 2-2im, -2 - 2im, -1.001-1.5im, -0.999-1.5im, -0.5-2im, -1-1.5im, -2+2im, -2, 0.1+0.2im, 0.1+im, 0.1-im, 1+0.1im, -1+0.1im, 1+im, -1+im, -1-im, 1-im)
+n = 6
+for z in Z
+    L = logkernelsquare(z,n)
+    for j = 0:n-1, k=0:n-j-1
+        q = quadgk(s -> quadgk(t -> iszero(s+im*t) ? 0.0+0.0im : log(z-(s+im*t)) * legendrep(k,s) * legendrep(j,t), -1, 1, atol=1E-12)[1], -1, 1, atol=1E-12)[1]
+        @test q ≈ L[k+1,j+1] atol=1E-10
+    end
+end
 
-logkernelsquare(big(z), n)
